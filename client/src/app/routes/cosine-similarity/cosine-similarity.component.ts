d5e972d35b0a2af16e0ba5841741d560f5d4e101
Peter Schramm <peter_schramm@live.de>
2023-09-15 15:37:34 +0200

    Added value indicator

--- client/src/app/routes/cosine-similarity/cosine-similarity.component.ts
+++ client/src/app/routes/cosine-similarity/cosine-similarity.component.ts
@@ -15,16 +15,26 @@ export class CosineSimilarityComponent implements AfterViewInit {
       resize: { enabled: false },
       axis: true,
     } as any);
+
+    // Draw cosine
     let func = function (x: number) { return Math.cos(x); };
     const funcStart = 0;
     const funcEnd = 2 * Math.PI;
+    let plot = board.create('functiongraph', [func, funcStart, funcEnd]);
+
     const sliderStartPos = [0, 0];
     const sliderEndPos = [2 * Math.PI, 0];
     const sliderStartValue = 0;
     const sliderEndValue = 2;
     const sliderValue = 2;
-    let slider = board.create('slider', [sliderStartPos, sliderEndPos, [sliderStartValue, sliderValue, sliderEndValue]], { snapWidth: 0.05, withLabel: false });
-    let plot = board.create('functiongraph', [func, funcStart, funcEnd]);
+    let slider = board.create('slider', [sliderStartPos, sliderEndPos, [sliderStartValue, sliderValue, sliderEndValue]], { snapWidth: 0.01, withLabel: false });
+
+    // cosine value indicator
+    const startLine = [() => slider.Value() * Math.PI, 0];
+    const endLine = [() => slider.Value() * Math.PI, () => Math.cos(slider.Value() * Math.PI)];
+    board.create('line', [startLine, endLine], { straightFirst: false, straightLast: false, strokeColor: '#aaaaaa' })
+
+    // equation
     board.create('text', [4.5, -0.7,
       function () {
         return `\\[cos(${slider.Value().toFixed(2)}\\pi)=${func(slider.Value() * Math.PI).toFixed(2)}\\]`;
